75f5a7a30376da25f70b29ac140a5d013d3e8184
Janet Chen <17347608+janet-chen@users.noreply.github.com>
2019-08-01 11:03:33 -0700

    example with video (autoplay, loop)

--- ar.js
+++ ar.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
          title: 'Toolbox'
    }) 
 });
+
+var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+  if (myVideo.paused) 
+    myVideo.play(); 
+  else 
+    myVideo.pause(); 
+} 
