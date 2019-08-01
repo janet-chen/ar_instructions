fca490d829220daed4b4f51c910ed20e60a6dd9a
Janet Chen <17347608+janet-chen@users.noreply.github.com>
2019-08-01 10:43:05 -0700

    popover images included

--- ar.js
+++ ar.js
@@ -1,3 +1,11 @@
-$(function () {
-    $('[data-toggle="popover"]').popover()
-})
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+         //trigger: 'focus',
+         trigger: 'hover',
+         html: true,
+         content: function () {
+               return  '<p> '+$(this).data('text')+' </p>' + '<img class="img-fluid" src="'+$(this).data('img') + '" />';
+         },
+         title: 'Toolbox'
+   }) 
+});
